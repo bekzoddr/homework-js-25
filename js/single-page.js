3ee441c45179f73fc85cb510075b04dd7c12fbe5
bekzoddr <149781755+bekzoddr@users.noreply.github.com>
2024-03-15 13:55:10 +0500

    single-page

--- js/single-page.js
+++ js/single-page.js
@@ -1,20 +1,20 @@
-const AIP_URL = "https://fakestoreapi.com/products/"
-const podactImage = document.querySelector(".main__image")
+const API_URL = "https://fakestoreapi.com/products/"
+const mainProduct = document.querySelector(".main__image")
 
 
 async function fetchSingleUser(api) {
     let params = new URLSearchParams(window.location.search)
     let id = params.get("id")
 
-    let data = await fetch(`${api} / ${id}`)
+    let data = await fetch(`${api}/${id}`)
     data
         .json()
         .then(res => SingleUser(res))
         .catch(err => console.log(err))
 }
 
-fetchSingleUser(AIP_URL)
+fetchSingleUser(API_URL)
 
-function SingleUser({id, image }) {
-    podactImage.src = image
+function SingleUser({image}) {
+    mainProduct.src = image
 }
